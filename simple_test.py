3c15246dbef99e5693c73b17ade9ca5b25d0e88e
AlexSlav <alexander.slavutin@vicuesoft.com>
2020-04-23 09:05:47 +0300

    fixed

--- simple_test.py
+++ simple_test.py
@@ -28,5 +28,5 @@ def test_wallet_spend_cash(wallet):
     assert wallet.balance == 10
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
-    #with pytest.raises(InsufficientAmount):
-        empty_wallet.spend_cash(100)
+    # with pytest.raises(InsufficientAmount):
+    empty_wallet.spend_cash(100)
